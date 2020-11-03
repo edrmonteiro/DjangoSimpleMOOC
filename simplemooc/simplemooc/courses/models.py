5f59a355d8358b935862291c0366347b123bf3c7
Eduardo Monteiro <eduardormonteiro@gmail.com>
2020-11-03 19:27:34 -0300

    add application and model for courses

---
+++ simplemooc/simplemooc/courses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField('Name', max_length=100)
+    slug = models.SlugField('Shortcut')
+    description = models.TextField('Description', blank=True)
+    start_date = models.DateField(
+        'Start date', null=True, blank=True
+    )
+    image = models.ImageField(
+        upload_to = 'courses/images', 
+        verbose_name='Image',
+        null=True, blank=True
+    )
+    created_at = models.DateTimeField("Created at", auto_now_add=True)
+    updated_at = models.DateTimeField("Created at", auto_now=True)
