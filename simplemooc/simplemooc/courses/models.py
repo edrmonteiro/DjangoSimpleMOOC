18ce12316464bcc76f6253818bc4e9ae2eba14ff
Eduardo Monteiro <eduardormonteiro@gmail.com>
2020-11-03 21:33:32 -0300

    finishing Courses model and admin

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description__icontains=query)
+        )
+
+
 class Course(models.Model):
     name = models.CharField('Name', max_length=100)
     slug = models.SlugField('Shortcut')
@@ -12,5 +21,15 @@ class Course(models.Model):
         verbose_name='Image',
         null=True, blank=True
     )
-    created_at = models.DateTimeField("Created at", auto_now_add=True)
-    updated_at = models.DateTimeField("Created at", auto_now=True)
+    created_at = models.DateTimeField("Created at", auto_now_add=True)  #when the object is created
+    updated_at = models.DateTimeField("Created at", auto_now=True)      #when there is any change in the object
+
+    objects = CourseManager()
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+        ordering = ['name']
